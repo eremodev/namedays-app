8870a306c23c0aad80b5ca9dd8eac99342bc78c3
eremodev <eremo.dev@gmail.com>
2021-02-16 22:14:29 +0100

    created artisan command Namedays for download/update table namedays

---
+++ app/Console/Commands/Namedays.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Nameday;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\GuzzleHttp;
+use GuzzleHttp\Client;
+
+class Namedays extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $client = new Client(['base_uri' => 'https://api.eremo.site/json/meniny.json']);
+        $response = $client->request('GET');
+        $mydata = json_decode($response->getBody()->getContents(), true);
+
+        foreach ($mydata['namedays'] as $data) {
+            Nameday::updateOrCreate([
+                'month' => $data['month'],
+                'date' => $data['date'],
+                'name' => $data['name'],
+            ]);
+        }
+    }
+}
