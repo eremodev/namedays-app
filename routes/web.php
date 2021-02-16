4d07cc4126f51c4114fb1c071029d8744a2073c5
eremodev <eremo.dev@gmail.com>
2021-02-16 22:16:43 +0100

    added routes for controller Namedays

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [NamedaysController::class, 'index']);
+Route::get('/namedays/{nameday}-{slug}', [NamedaysController::class, 'show']);
+Route::get('/autocomplete', [NamedaysController::class, 'autocomplete'])->name('autocomplete');
