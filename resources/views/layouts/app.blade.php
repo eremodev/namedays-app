7715a9c14696a27b3fdc39dab7ecc4209816caaa
eremodev <eremo.dev@gmail.com>
2021-02-16 21:59:02 +0100

    created app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	<title>{{ config('app.name') }}</title>
+</head>
+<body>
+    <main class="section">
+		@yield('content')
+	</main>
+</body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
+<script src="https://kit.fontawesome.com/1261b499ba.js" crossorigin="anonymous"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
